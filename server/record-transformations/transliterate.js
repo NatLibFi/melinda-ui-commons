c90259a813357726d6edd1754890c1bf44b393fd
Pasi Tuominen <pasi.tuominen@gmail.com>
2016-11-01 18:41:25 +0200

    Adds initial working version of transliteration functionality

--- server/record-transformations/transliterate.js
+++ server/record-transformations/transliterate.js
@@ -1,3 +1,6 @@
+import _ from 'lodash';
+import * as sfs4900 from 'sfs4900';
+import iso9 from 'iso_9';
 
 const cyrillicCharacters = [
   'А', 'а', 'Б', 'б', 'В', 'в', 'Г', 'г', 'Д', 'д', 'Е', 'е', 'Ё', 'ё', 'Ж', 
@@ -13,29 +16,209 @@ function isCyrillicCharacter(char) {
 function containsCyrillicCharacters(str) {
   return str.split('').some(isCyrillicCharacter);
 }
-
-
-
-function shouldConvertField(field) {
-
+function fieldContainsCyrillicCharacters(field) {
   return field.subfields && field.subfields.map(sub => sub.value).some(containsCyrillicCharacters);
 }
 
-export function transliterate(record, opts) {
+function shouldConvertField(field) {
+  return field.tag !== '880' && fieldContainsCyrillicCharacters(field);
+}
+function shouldCreateTransliteratedFields(field) {
+  return field.tag === '880' && fieldContainsCyrillicCharacters(field);
+}
 
-  return new Promise((resolve, reject) => {
+export function transliterate(record) {
 
-    record.fields = record.fields.map(field => {
+  return new Promise((resolve) => {
+
+    record.fields = record.fields.reduce((fields, field) => {
+
       if (shouldConvertField(field)) {
-        if (field.tag === '100') {
-          field.tag = '880';
-        }
-      }
-      return field;
-    });
 
+        const tagForLinking = field.tag;
+        const linkNumber = getNextAvailableLinkNumber(_.concat(record.fields, fields));
+
+        const origField = _.assign({}, field, {
+          tag: '880'
+        });
+        origField.subfields.unshift({code: '6', value: `${tagForLinking}-${linkNumber}`});
+       
+        const isThereAlready = _.partial(fieldIsLessOrEqual, origField);
+        if (record.fields.some(isThereAlready)) {
+         
+          fields.push(field);
+        } else {
+         
+          fields.push(origField);  
+        }
+        
+        
+      } else {
+        fields.push(field);  
+      }
+      return fields;
+    }, []);
+
+    const removeFields = [];
+
+    record.fields = record.fields.reduce((fields, field) => {
+
+      if (shouldCreateTransliteratedFields(field)) {
+        
+        const link = field.subfields.find(sub => sub.code === '6').value;
+        const [linkedTag, linkNumber] = link.split('-');
+
+
+        const sfs4900Transliterated = _.assign({}, field, {
+          tag: '880',
+          subfields: field.subfields
+            .filter(sub => sub.code !== '6')
+            .filter(sub => !(sub.code === '9' && sub.value.indexOf('<TRANS>') !== -1))
+            .map(transliterateSubfield('sfs4900'))
+        });
+        sfs4900Transliterated.subfields.unshift({code: '6', value: `${linkedTag}-${linkNumber}`});
+        sfs4900Transliterated.subfields.push({code: '9', value: 'SFS4900 <TRANS>'});
+        
+        const isThereAlready2 = _.partial(fieldIsLessOrEqual, sfs4900Transliterated);
+        if (!record.fields.some(isThereAlready2)) {
+          fields.push(sfs4900Transliterated);
+        }
+
+
+        const iso9Transliterated = {
+          tag: linkedTag,
+          ind1: field.ind1,
+          ind2: field.ind2,
+          subfields: field.subfields
+            .filter(sub => sub.code !== '6')
+            .filter(sub => !(sub.code === '9' && sub.value.indexOf('<TRANS>') !== -1))
+            .map(transliterateSubfield('iso9'))
+        };
+        const iso9TransliteratedLinkSubField = {code: '6', value: `880-${linkNumber}`};
+        iso9Transliterated.subfields.unshift(iso9TransliteratedLinkSubField);
+        iso9Transliterated.subfields.push({code: '9', value: 'ISO9 <TRANS>'});
+
+        const isThereAlready = _.partial(fieldIsLessOrEqual, iso9Transliterated);
+        if (!record.fields.some(isThereAlready)) {
+          fields.push(iso9Transliterated);
+
+          record.fields
+            .filter(field => field.tag == iso9Transliterated.tag)
+            .filter(field => field.subfields.some(sub => _.isEqual(sub, iso9TransliteratedLinkSubField)))
+            .forEach(field => {
+              removeFields.push(field);    
+            });
+        }
+
+      
+
+        const cyrillicNoteSubField = {code: '9', value: 'CYRILLIC <TRANS>'};
+        if (!field.subfields.some(sub => _.isEqual(sub, cyrillicNoteSubField))) {
+          field.subfields.push(cyrillicNoteSubField);
+        }
+        
+      }
+      fields.push(field);
+
+      return fields;
+    }, []);
+
+    record.fields = _.difference(record.fields, removeFields);
+
+    record.fields = sortNumericFields(record.fields);
     resolve(record);
 
+
+  });
+}
+
+function fieldIsLessOrEqual(fieldA, fieldB) {
+  if (fieldA.tag !== fieldB.tag) return false;
+  if (fieldA.ind1 !== fieldB.ind1) return false;
+  if (fieldA.ind2 !== fieldB.ind2) return false;
+  if (typeof fieldA.subfields !== typeof fieldB.subfields) return false;
+
+  if (fieldA.subfields) {
+    if (_.differenceWith(fieldA.subfields, fieldB.subfields, _.isEqual).length !== 0) return false;  
+  } else {
+    if (fieldA.value !== fieldB.value) return false;
+  }
+  
+  return true;
+}
+
+
+function sortNumericFields(fields) {
+  const nonNumericFields = fields.reduce((acc, field, i) => {
+    if (isNaN(field.tag)) {
+      acc.push({
+        field: field,
+        index: i
+      });
+    }
+    return acc;
+  }, []);
+
+  const numericFields = _.without(fields, nonNumericFields.map(item => item.field));
+
+  numericFields.sort((a, b) => {
+    const byTag = parseInt(a.tag) - parseInt(b.tag);
+    if (byTag !== 0) return byTag;
+    const [aLinkedTag, ] = getLink(a);
+    const [bLinkedTag, ] = getLink(b);
+
+    const byLinkedFieldTag = parseInt(aLinkedTag) - parseInt(bLinkedTag);
+    if (byLinkedFieldTag !== 0) return byLinkedFieldTag;
+
+    const cyrillicFirst = fieldContainsCyrillicCharacters(b) ? 1 : -1;
+    return cyrillicFirst;
   });
 
+  nonNumericFields.forEach(item => {
+    numericFields.splice(item.index, 0, item.field);
+  });
+
+  return numericFields;
+}
+
+function getNextAvailableLinkNumber(fields) {
+  const currentLinkNumbers = _(fields)
+    .flatMap(field => field.subfields)
+    .filter(_.isObject)
+    .filter(sub => sub.code === '6')
+    .map(sub => sub.value)
+    .map(link => _.last(link.split('-')))
+    .value();
+
+  const isTaken = (linkNumberPart) => {
+    return currentLinkNumbers.some(used => used === linkNumberPart);
+  };
+
+  return _.range(1,100).map(num => _.padStart(num, 2, '0')).find(linkNumberPart => !isTaken(linkNumberPart));
+}
+
+function getLink(field) {
+  const links = _.get(field, 'subfields', [])
+    .filter(sub => sub.code === '6')
+    .map(sub => sub.value)
+    .map(link => link.split('-'));
+
+  return _.head(links);
+}
+
+function transliterateSubfield(type) {
+  return function(subfield) {
+    const converted = type === 'sfs4900' ? sfs4900Convert(subfield.value) : iso9Convert(subfield.value);
+    return _.assign({}, subfield, {value: converted.result });
+  };
+}
+
+function sfs4900Convert(str) {
+  return sfs4900.convertToLatin(str);
+}
+
+function iso9Convert(str) {
+  return {
+    result: iso9(str, 1)
+  }; 
 }
--- server/record-transformations/transliterate.spec.js
+++ server/record-transformations/transliterate.spec.js
@@ -14,21 +14,8 @@ export const FAKE_RECORD_CYRILLIC_AUTHOR = MarcRecord.fromString(
 100    ‡aЧайковский`);
 
 
-describe.only('transliterate', () => {
+describe('transliterate', () => {
   let result;
-  let error;
-
-  describe('when record does not contain any cyrillic characters', () => {
-    beforeEach(() => {
-      return transliterate(copy(FAKE_RECORD), {})
-        .then(res => result = res)
-        .catch(err => error = err);
-    });
-
-    it('keeps the record intact', () => {
-      expect(result.fields).to.eql(FAKE_RECORD.fields);
-    });
-  });
 
   describe('when record has cyrillic author in field 100', () => {
     let field100;
@@ -37,7 +24,7 @@ describe.only('transliterate', () => {
     beforeEach(() => {
       return transliterate(copy(FAKE_RECORD_CYRILLIC_AUTHOR), {})
         .then(res => result = res)
-        .catch(err => error = err)
+        .catch(err => { throw err; })
         .then(() => {
           field100 = _.head(result.fields.filter(field => field.tag === '100'));
           fields880 = result.fields.filter(field => field.tag === '880');
@@ -45,7 +32,7 @@ describe.only('transliterate', () => {
     });
 
     it('creates ISO-9 transliterated version and puts it into field 100 (author field)', () => {
-      expect(subfield('a').of(field100)).to.equal('Tchaikovsky');
+      expect(subfield('a').of(field100)).to.equal('Čajkovskij');
     });
 
     it('creates link subfield $6 to field 100 for linking to originial text', () => {
@@ -81,8 +68,44 @@ describe.only('transliterate', () => {
     });
 
   });
-  
 
+  const parsed = getTransliterationTestStory()
+    .split('\n\n')
+    .map(s => s.trim())
+    .filter(testDef => testDef.length > 0)
+    .map(testDef => {
+      return {
+        heading: _.head(testDef.split(':\n')),
+        input: _.tail(testDef.split(':\n')).join(':\n')
+      };
+    })
+    .map(testDef => {
+      return _.assign({}, testDef, {
+        record: MarcRecord.fromString(testDef.input)
+      });
+    });
+
+  const tests = _.chunk(parsed, 2);
+
+  tests.forEach((test) => {
+
+    const testName = `${test[0].heading}`;
+
+    const testFn = () => {
+      return transliterate(test[0].record).then(result => {
+        expect(result.toString().split('\n')).to.eql(test[1].record.toString().split('\n'));
+      });
+    };
+
+
+    if (test[0].heading.startsWith('!')) {
+      it.only(testName, testFn);
+    } else {
+      it(testName, testFn);
+    }
+
+  });
+  
 });
 
 function copy(record) {
@@ -107,4 +130,192 @@ function subfield(code) {
   function getFirstSubfieldValue(field, code) {
     return _.head(field.subfields.filter(sub => sub.code === code).map(sub => sub.value));
   }
+}
+
+function getTransliterationTestStory() {
+  return `
+should do nothing if there is no cyrillic characters:
+LDR    abcdefghijk
+001    28474
+100    ‡aTest Author
+
+after applying transliteration:
+LDR    abcdefghijk
+001    28474
+100    ‡aTest Author
+
+
+should create transliterated fields:
+LDR    abcdefghijk
+001    28474
+100    ‡aЧайковский
+
+after applying transliteration:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aČajkovskij‡9ISO9 <TRANS>
+880    ‡6100-01‡aЧайковский‡9CYRILLIC <TRANS>
+880    ‡6100-01‡aTšaikovski‡9SFS4900 <TRANS>
+
+
+should move indicators:
+LDR    abcdefghijk
+001    28474
+100  1 ‡aЧайковский
+
+after applying transliteration:
+LDR    abcdefghijk
+001    28474
+100  1 ‡6880-01‡aČajkovskij‡9ISO9 <TRANS>
+880  1 ‡6100-01‡aЧайковский‡9CYRILLIC <TRANS>
+880  1 ‡6100-01‡aTšaikovski‡9SFS4900 <TRANS>
+
+
+should handle transliteration with multiple subfields:
+LDR    abcdefghijk
+001    28474
+100    ‡aЧайковский‡bЧайковский
+
+after applying transliteration:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aČajkovskij‡bČajkovskij‡9ISO9 <TRANS>
+880    ‡6100-01‡aЧайковский‡bЧайковский‡9CYRILLIC <TRANS>
+880    ‡6100-01‡aTšaikovski‡bTšaikovski‡9SFS4900 <TRANS>
+
+
+should handle transliteration with multiple fields:
+LDR    abcdefghijk
+001    28474
+100    ‡aЧайковский
+245    ‡aЧайковский
+
+after applying transliteration:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aČajkovskij‡9ISO9 <TRANS>
+245    ‡6880-02‡aČajkovskij‡9ISO9 <TRANS>
+880    ‡6100-01‡aЧайковский‡9CYRILLIC <TRANS>
+880    ‡6100-01‡aTšaikovski‡9SFS4900 <TRANS>
+880    ‡6245-02‡aЧайковский‡9CYRILLIC <TRANS>
+880    ‡6245-02‡aTšaikovski‡9SFS4900 <TRANS>
+
+
+is idempotent:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aČajkovskij‡9ISO9 <TRANS>
+880    ‡6100-01‡aЧайковский‡9CYRILLIC <TRANS>
+880    ‡6100-01‡aTšaikovski‡9SFS4900 <TRANS>
+
+after applying transliteration:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aČajkovskij‡9ISO9 <TRANS>
+880    ‡6100-01‡aЧайковский‡9CYRILLIC <TRANS>
+880    ‡6100-01‡aTšaikovski‡9SFS4900 <TRANS>
+
+
+handles mixed transliterated content:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aČajkovskij‡9ISO9 <TRANS>
+245    ‡aЧайковский
+880    ‡6100-01‡aЧайковский‡9CYRILLIC <TRANS>
+880    ‡6100-01‡aTšaikovski‡9SFS4900 <TRANS>
+
+after applying transliteration:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aČajkovskij‡9ISO9 <TRANS>
+245    ‡6880-02‡aČajkovskij‡9ISO9 <TRANS>
+880    ‡6100-01‡aЧайковский‡9CYRILLIC <TRANS>
+880    ‡6100-01‡aTšaikovski‡9SFS4900 <TRANS>
+880    ‡6245-02‡aЧайковский‡9CYRILLIC <TRANS>
+880    ‡6245-02‡aTšaikovski‡9SFS4900 <TRANS>
+
+
+completes partial transliteration from SFS4900:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aTšaikovski
+880    ‡6100-01‡aЧайковский
+
+after applying transliteration:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aČajkovskij‡9ISO9 <TRANS>
+880    ‡6100-01‡aЧайковский‡9CYRILLIC <TRANS>
+880    ‡6100-01‡aTšaikovski‡9SFS4900 <TRANS>
+
+
+completes multiple partial transliterations from SFS4900:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aTšaikovski
+245    ‡6880-02‡aTšaikovski
+880    ‡6100-01‡aЧайковский
+880    ‡6245-02‡aЧайковский
+
+after applying transliteration:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aČajkovskij‡9ISO9 <TRANS>
+245    ‡6880-02‡aČajkovskij‡9ISO9 <TRANS>
+880    ‡6100-01‡aЧайковский‡9CYRILLIC <TRANS>
+880    ‡6100-01‡aTšaikovski‡9SFS4900 <TRANS>
+880    ‡6245-02‡aЧайковский‡9CYRILLIC <TRANS>
+880    ‡6245-02‡aTšaikovski‡9SFS4900 <TRANS>
+
+
+completes partial transliteration from ISO9:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aČajkovskij
+880    ‡6100-01‡aЧайковский
+
+after applying transliteration:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aČajkovskij‡9ISO9 <TRANS>
+880    ‡6100-01‡aЧайковский‡9CYRILLIC <TRANS>
+880    ‡6100-01‡aTšaikovski‡9SFS4900 <TRANS>
+
+
+should not drop 9-subfields that have nothing to do with transliteration:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aČajkovskij‡9ALMA <KEEP>
+880    ‡6100-01‡aЧайковский‡9ALMA <KEEP>
+
+after applying transliteration:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aČajkovskij‡9ALMA <KEEP>‡9ISO9 <TRANS>
+880    ‡6100-01‡aЧайковский‡9ALMA <KEEP>‡9CYRILLIC <TRANS>
+880    ‡6100-01‡aTšaikovski‡9ALMA <KEEP>‡9SFS4900 <TRANS>
+
+
+does not change record with broken links:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aČajkovskij
+
+after applying transliteration:
+LDR    abcdefghijk
+001    28474
+100    ‡6880-01‡aČajkovskij
+
+
+does not handle cyrillic characters in leader or fixed fields:
+LDR    abČdefghijk
+001    28474
+008    ČČČČČČČČČČČČČČČČ
+
+after applying transliteration:
+LDR    abČdefghijk
+001    28474
+008    ČČČČČČČČČČČČČČČČ
+`;
+
 }
