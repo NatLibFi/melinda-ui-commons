b26a81bdb7f47e495690d6c5e8eb27b50e4793ae
Pasi Tuominen <pasi.tuominen@gmail.com>
2016-11-14 11:20:04 +0200

    .only removal

--- server/record-transformations/remove-local-reference.spec.js
+++ server/record-transformations/remove-local-reference.spec.js
@@ -4,7 +4,7 @@ import { removeLocalReference } from './remove-local-reference';
 import { FAKE_RECORD_FCC_SID, FAKE_RECORD, FAKE_DELETED_RECORD, FAKE_RECORD_SID_LOW, FAKE_RECORD_FOR_CLEANUP, FAKE_RECORD_ONLY_LOW_TEST, FAKE_RECORD_2_LOW } from '../test_helpers/fake-data';
 import { exceptCoreErrors } from '../utils';
 
-describe.only('removeLocalReference', () => {
+describe('removeLocalReference', () => {
 
   const LIBRARY_TAG = 'test';
   const EXPECTED_LOCAL_ID = 111;
