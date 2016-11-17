fd0697e7d8a474c416f790a325ae096f68445ee2
Pasi Tuominen <pasi.tuominen@gmail.com>
2016-11-17 21:41:04 +0200

    fake required variables

--- server/test_helpers/test_bootstrap.js
+++ server/test_helpers/test_bootstrap.js
@@ -3,3 +3,5 @@ process.env.ALEPH_USER_LIBRARY='test-lib';
 process.env.DUPLICATE_DB_URL='test-duplicate-db-url';
 process.env.AMQP_HOST='test-amqp-host';
 process.env.SMTP_CONNECTION_URL='smtp://test:test@localhost:2525';
+process.env.USEMARCON_BIN=process.env.USEMARCON_BIN || '/path/to/bin';
+process.env.USEMARCON_CONFIG_PATH=process.env.USEMARCON_BIN || '/tmp';
