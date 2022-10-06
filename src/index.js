33e1b06cc2c3cf850f53dc599e354a6823477576
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2022-10-06 14:59:59 +0300

    fix auth

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import * as account from './auth';
+import * as auth from './auth';
 import * as idbOperator from './indexDB';
 import {showRecord} from './marc-record-ui';
 import {setNavBar, startProcess, stopProcess} from './navbar';
 import * as utils from './ui-utils';
 
-export {account, idbOperator, showRecord, setNavBar, startProcess, stopProcess, utils};
+export {auth, idbOperator, showRecord, setNavBar, startProcess, stopProcess, utils};
