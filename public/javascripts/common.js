caacd2baa1760ef7e02c5488bdc04e49cec8aa9b
natlibfi-ptalosel <pinja.talosela@helsinki.fi>
2023-10-16 14:39:53 +0300

    Shared partials and public

---
+++ public/javascripts/common.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+
+async function doRestCall({url = undefined, method = undefined, body = undefined, contentType = undefined, resultAsJson = false}) {
+
+  const headers = {
+    'Accept': 'application/json',
+    ...contentType ? {'Content-Type': contentType} : {}
+  };
+
+  const result = await fetch(
+    url,
+    {
+      method,
+      headers,
+      ...body ? {body} : {}
+    }
+  );
+
+  if (resultAsJson) {
+    return result.json();
+  }
+
+  return result;
+}
+
+window.doRestCall = doRestCall;
+
