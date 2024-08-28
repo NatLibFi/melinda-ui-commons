61d337a15353749804f7c34a8233a24fff2548fb
natlibfi-ptalosel <117168919+natlibfi-ptalosel@users.noreply.github.com>
2024-08-28 11:52:14 +0300

    Update accordion component

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -13,3 +13,19 @@ export function toggleAccordion(event) {
 
   return eventHandled(event);
 }
+
+export function getAllAccordions(parentElement) {
+  if (parentElement) {
+    return parentElement.querySelectorAll('.accordion-heading');
+  }
+
+  document.querySelectorAll('.accordion-heading');
+}
+
+export function addToggleAccordionEventListeners(accordions) {
+  accordions.forEach(accordion => {
+    accordion.addEventListener('click', event => {
+      toggleAccordion(event);
+    });
+  })
+}
