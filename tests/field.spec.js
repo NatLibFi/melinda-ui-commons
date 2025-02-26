cc82021010e47c7a2796f23fd7332a9d6e33a45b
nvolk <nicholas.volk@helsinki.fi>
2025-02-26 14:31:25 +0200

    Add first test using jsdom (when testing marc field to html conversion)

---
+++ tests/field.spec.js
@@ -0,0 +1,42 @@
+import {expect} from 'chai';
+import fixugen from '@natlibfi/fixugen';
+
+//import {MarcRecord} from '@natlibfi/marc-record';
+//import createReducer from './postprocessor';
+import {READERS} from '@natlibfi/fixura';
+const {default: generateTests} = fixugen;
+
+import {marcFieldToDiv} from '../src/scripts/marcRecordUi.js';
+
+import {JSDOM} from 'jsdom';
+
+const dom = new JSDOM('<!DOCTYPE html><div id="editor"></div>');
+const document = dom.window.document;
+
+describe('editor (whole record) ', () => {
+  generateTests({
+    callback,
+    path: [import.meta.dirname, 'testFixtures', 'field'],
+    recurse: false,
+    useMetadataFile: true,
+    fixura: {
+      failWhenNotFound: false,
+      reader: READERS.TEXT
+    }
+  });
+
+  function callback({getFixture, decorator = {}}) {
+    const inputField = JSON.parse(getFixture('field.json'));
+    //console.log(JSON.stringify(inputField));
+    const targetHtml = getFixture('target.html');
+    //console.log(targetHtml);
+
+    const elem = document.getElementById('editor');
+    elem.innerHTML = '';
+
+    const outputHtml = marcFieldToDiv(elem, inputField, decorator, true, document).innerHTML.replace(/></g, '>\n<');
+
+    expect(outputHtml).to.equal(targetHtml);
+  }
+
+});
